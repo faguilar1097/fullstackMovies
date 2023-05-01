90f2cd9bf243eb7e5b38be3fe5cd62ddd803fc60
Francisco Aguilar <f.aguilar.work97@gmail.com>
2023-05-01 11:02:10 -0600

    Subiendo el ejercicio completo back+front

---
+++ movies-server/src/test/java/com/faguilar97/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faguilar97.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
